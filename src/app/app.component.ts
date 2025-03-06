61e697db9b6cb2c8a86351419918d66573553219
Kushagra Gupta <kushagragupta853@gmail.com>
2025-03-06 15:58:18 +0530

    [Kushagra] Update. Display Hello World using interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Hello from BridgeLabz';
+
+  ngOnInit(): void {
+    this.title = "Hello from BridgeLabz."
+  }
 }
