4986c9a0ac3fc35e25cd61ecc398d7be1e73efca
Kushagra Gupta <kushagragupta853@gmail.com>
2025-03-07 11:29:12 +0530

    [Kushagra] files added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  imports : [FormsModule],
   standalone: true
 })
 export class AppComponent {
   title = 'Hello from BridgeLabz';
   imgUrl = "assets/BL_logo_square_jpg.jpg";
   url = "https://www.bridgelabz.com";
+  userName: string = "";
 
   ngOnInit(): void {
     this.title = "Hello from BridgeLabz."
